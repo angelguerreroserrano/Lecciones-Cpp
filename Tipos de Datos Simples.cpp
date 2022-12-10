31819367d38d4c4451029cfae28fe9046d3ed1f6
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2022-12-10 21:21:51 +0100

    Add files via upload

---
+++ Tipos de Datos Simples.cpp
@@ -0,0 +1,15 @@
+//Variables de C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int entero = 5;
+	float decimal = 4.3;
+	
+	cout<<entero<<endl;
+	cout<<decimal<<endl;
+	
+	return 0;
+}
