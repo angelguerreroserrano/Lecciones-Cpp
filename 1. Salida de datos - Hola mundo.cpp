872238a39c730edc44cc454207513d253cfa1273
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2022-12-10 21:35:01 +0100

    Update 1. Salida de datos - Hola mundo.cpp

--- 1. Salida de datos - Hola mundo.cpp
+++ 1. Salida de datos - Hola mundo.cpp
@@ -1,10 +1,12 @@
-//Primer programa en C++
-//Este programa imprime 'Hola mundo :)'.
-//C++ necesita una librería y una función para poder funcionar. La librería básica para poder meter y sacar datos es iostream (Input-Output-Stream).
-//Para facilitar las cosas ponemos 'using namespace std'. Sino, habría que poner std:: antes de cada cout.
-//Para imprimir algo se usa cout.
-//Para finalizar una funcion hace falta un return.
-//Después de cada línea hace falta cerrarla con ;
+/*
+Primer programa en C++
+Este programa imprime 'Hola mundo :)'.
+C++ necesita una librería y una función para poder funcionar. La librería básica para poder meter y sacar datos es iostream (Input-Output-Stream).
+Para facilitar las cosas ponemos 'using namespace std'. Sino, habría que poner std:: antes de cada cout.
+Para imprimir algo se usa cout.
+Para finalizar una funcion hace falta un return.
+Después de cada línea hace falta cerrarla con ;
+*/
 
 #include<iostream>
 
